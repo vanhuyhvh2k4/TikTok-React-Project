77a6ec19923d620030ebbaf812432b3a745ff2b1
anhhuydepzai <anhhuydepzai@gmail.com>
2023-03-05 20:37:36 +0700

    inital tiktok project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
 
 function App() {
-  const [counter, setCounter] = useState(1);
-  const handleIncrease = () => {
-    setCounter(counter => counter + 1)
-  }
   
   return (
     <div className="App">
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+      
     </div>
   );
 }
